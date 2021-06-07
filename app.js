eff99ab5bbb5d5f78fd13a8050372b942496275e
chagy <chagy_x_@hotmail.com>
2021-06-07 21:05:37 +0700

    117

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3003;
-const middleware = require('./middleware')
-const path = require('path')
-const bodyParser = require("body-parser")
+const middleware = require("./middleware");
+const path = require("path");
+const bodyParser = require("body-parser");
 const mongoose = require("./database");
 const session = require("express-session");
 
-const server = app.listen(port, () => console.log("Server listening on port " + port));
+const server = app.listen(port, () =>
+  console.log("Server listening on port " + port)
+);
 
 app.set("view engine", "pug");
 app.set("views", "views");
@@ -15,31 +17,34 @@ app.set("views", "views");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use(session({
+app.use(
+  session({
     secret: "bbq chips",
     resave: true,
-    saveUninitialized: false
-}))
+    saveUninitialized: false,
+  })
+);
 
 // Routes
-const loginRoute = require('./routes/loginRoutes');
-const registerRoute = require('./routes/registerRoutes');
+const loginRoute = require("./routes/loginRoutes");
+const registerRoute = require("./routes/registerRoutes");
+const postRoute = require("./routes/postRoutes");
 
 // Api routes
-const postsApiRoute = require('./routes/api/posts');
+const postsApiRoute = require("./routes/api/posts");
 
 app.use("/login", loginRoute);
 app.use("/register", registerRoute);
+app.use("/post", middleware.requireLogin, postRoute);
 
 app.use("/api/posts", postsApiRoute);
 
 app.get("/", middleware.requireLogin, (req, res, next) => {
+  var payload = {
+    pageTitle: "Home",
+    userLoggedIn: req.session.user,
+    userLoggedInJs: JSON.stringify(req.session.user),
+  };
 
-    var payload = {
-        pageTitle: "Home",
-        userLoggedIn: req.session.user,
-        userLoggedInJs: JSON.stringify(req.session.user),
-    }
-
-    res.status(200).render("home", payload);
-})
+  res.status(200).render("home", payload);
+});
